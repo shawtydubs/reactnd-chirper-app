ebaa333b9871df5ccfcf17f9fbba19098b0e1102
Claire Walters <claire.walters@opower.com>
2018-07-09 20:09:28 -0400

    Add loading bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,20 @@ import {getInitialData} from '../utils/api';
 import {receiveTweets} from './tweets';
 import {receiveUsers} from './users';
 import {setAuthedUser} from './authedUser';
+import {hideLoading, showLoading} from 'react-redux-loading';
 
 const AUTHED_ID = 'shawtydubs';
 
 export function handleInitialData () {
     return (dispatch) => {
+        dispatch(showLoading());
+
         return getInitialData()
             .then(({users, tweets}) => {
                 dispatch(receiveTweets(tweets));
                 dispatch(receiveUsers(users));
                 dispatch(setAuthedUser(AUTHED_ID));
+                dispatch(hideLoading());
             })
     };
 };
